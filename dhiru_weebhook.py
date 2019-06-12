f1c7eb4cb65c3aeacb5554d30ab252251e46356e
dhiraj-quantumsoftech <51738068+dhiraj-quantumsoftech@users.noreply.github.com>
2019-06-12 17:02:06 +0530

    Update dhiru_weebhook.py

--- dhiru_weebhook.py
+++ dhiru_weebhook.py
@@ -20,6 +20,44 @@ from flask import make_response
 app = Flask(__name__)
 
 
+# -------------------- My Test -----------------
+
+@app.route('/dhiru_post',methods=['GET'])
+def dhiru_post():
+    req = request.get_json(silent=True, force=True)
+        
+        res = processDhiruRequest(req)
+            
+            res = json.dumps(res, indent=4)
+                
+                r = make_response(res)
+                    
+                    r.headers['Content-Type'] = 'application/json'
+
+return r
+
+def processDhiruRequest(req):
+ 
+    baseurl = 'https://www.nayaindia.com/api_new/'
+    
+    parameters =   {
+   "api_type":"news_listing",
+    "slug":"life-mantra",
+    "start":0,
+         "length":12
+
+}
+    
+    response = requests.post(
+            baseurl, data=json.dumps(parameters),
+            headers={'Content-Type': 'application/json'}
+        )
+        return response.content
+
+#-----------------------------------------------------
+
+
+
 @app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
